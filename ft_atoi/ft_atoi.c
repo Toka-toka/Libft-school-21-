434e381e908e82613c1c7eafa4dec96ec542010c
Toka_toka <mos9101@mail.ru>
2020-05-06 16:44:51 +0300

    over long, norme!

--- ft_atoi/ft_atoi.c
+++ ft_atoi/ft_atoi.c
@@ -6,31 +6,34 @@
 /*   By: sedric <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/30 19:12:54 by sedric            #+#    #+#             */
-/*   Updated: 2020/04/30 21:51:44 by sedric           ###   ########.fr       */
+/*   Updated: 2020/05/06 16:29:37 by sedric           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *str)
 {
-	int i;
-	int number;
-	int flag;
+	int				i;
+	int				flag;
+	unsigned long	number;
 
 	i = 0;
 	number = 0;
 	flag = 1;
-	while (str[i] >= 9 && str[i] <= 13 || str[i] == 32)
-		i++;
-	if (str[i] == 43 || str[i] == 45)
+	while (*str >= 9 && *str <= 13 || *str == 32)
+		str++;
+	if (*str == 43 || *str == 45)
 	{
-		if (str[i] == 45)
+		if (*str == 45)
 			flag = -1;
-		i++;
+		str++;
 	}
-	while (str[i] >= 48 && str[i] <= 57)
+	while (*str >= 48 && *str <= 57)
 	{
-		number = str[i] - 48 + number * 10;
+		number = *str - 48 + number * 10;
 		i++;
+		str++;
+		if (number > 9223372036854775807 || i >= 20)
+			return (-0.5 * (flag + 1));
 	}
 	return (number * flag);
 }

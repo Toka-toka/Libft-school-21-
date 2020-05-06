ebb8d5829564f9b6a9c0205573c35cf07cbe6619
Toka_toka <mos9101@mail.ru>
2020-05-06 19:34:08 +0300

    Main change

--- ft_memset/main_memset.c
+++ ft_memset/main_memset.c
@@ -6,23 +6,21 @@
 /*   By: sedric <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/06 17:15:05 by sedric            #+#    #+#             */
-/*   Updated: 2020/05/06 18:35:17 by sedric           ###   ########.fr       */
+/*   Updated: 2020/05/06 19:22:50 by sedric           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
+# include <string.h>
+# include <stdio.h>
 
 void    *ft_memset(void *str, int symbol, size_t n);
 
 int main (void)
 {
 	char str[] = "12345";
-	int symbol = 'x';
-	size_t n = 6;
-	ft_memset(str, symbol, n);
-//	memset(str, symbol, n);
-	printf("Size of str = %ld", sizeof(str));
-	printf("NEW = %s", str);
+	int symbol = 45;
+	size_t n = 5;
+	memset(str, symbol, n);
+	printf("NEW = %s\n", str);
 	return (0); 
 }

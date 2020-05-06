53ae16c448aadd769d9f2d72718eb9264abb34ba
Toka_toka <mos9101@mail.ru>
2020-05-06 20:22:10 +0300

    memcpy done, norme ok!

---
+++ ft_memcpy/ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sedric <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/06 17:13:04 by sedric            #+#    #+#             */
+/*   Updated: 2020/05/06 20:04:02 by sedric           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char *dest_point;
+	char *src_point;
+
+	dest_point = (char*)dest;
+	src_point = (char*)src;
+	while (n > 0)
+	{
+		*dest_point++ = *src_point++;
+		n--;
+	}
+	return (dest);
+}

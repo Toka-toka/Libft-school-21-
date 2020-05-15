1b3d0a7cf0d20fd998e3c47caa02da544dee3eb4
Toka_toka <mos9101@mail.ru>
2020-05-15 18:45:33 +0300

    replase, git ignore update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sedric <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/09 22:22:02 by sedric            #+#    #+#             */
-/*   Updated: 2020/05/13 17:33:47 by sedric           ###   ########.fr       */
+/*   Updated: 2020/05/14 21:30:58 by sedric           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,28 @@
 
 # include <stdio.h>
 
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memset(void *str, int symbol, size_t n);
+void	ft_bzero(void *str, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int symbol, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *arr, int symbol, size_t n);
-size_t	ft_strlcat(char *dest, const char *src, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dest, const char *src, size_t len_dest);
+size_t	ft_strlcat(char *dest, const char *src, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_isdigit(int c);
+int		ft_atoi(const char *str);
 int		ft_isalpha(int c);
+int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
+int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-int		ft_isprint(int c);
 int		ft_isalpha(int c);
 
 #endif

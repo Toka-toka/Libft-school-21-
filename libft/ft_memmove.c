314e71cce6bd7358699cb32078324f0b43f25c8f
Toka_toka <mos9101@mail.ru>
2020-05-16 16:32:09 +0300

    ok

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: sedric <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/06 17:13:04 by sedric            #+#    #+#             */
-/*   Updated: 2020/05/15 20:43:32 by sedric           ###   ########.fr       */
+/*   Updated: 2020/05/16 15:33:23 by sedric           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,24 @@ void				*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char	*dest_point;
 	unsigned char	*src_point;
-	unsigned char	temp[n];
-	size_t			i;
 
 	src_point = (unsigned char*)src;
 	dest_point = (unsigned char*)dest;
-	i = 0;
-	while (i < n)
+	if (src_point == NULL && dest_point == NULL)
+		return (NULL);
+	if (src > dest)
+		while (n != 0)
+		{
+			*dest_point++ = *src_point++;
+			n--;
+		}
+	else
 	{
-		temp[i] = src_point[i];
-		i++;
-	}
-	i = 0;
-	while (i < n)
-	{
-		dest_point[i] = temp[i];
-		i++;
+		while (n != 0)
+		{
+			dest_point[n-1] = src_point[n-1];
+			n--;
+		}
 	}
 	return (dest);
 }

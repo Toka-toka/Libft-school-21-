fdf53b11b4eb0997a375e2472f38205307403cb1
Toka_toka <mos9101@mail.ru>
2020-05-18 10:19:36 +0300

    itoa works and norme - ok

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,35 +6,30 @@
 /*   By: sedric <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/17 22:46:04 by sedric            #+#    #+#             */
-/*   Updated: 2020/05/18 00:54:03 by sedric           ###   ########.fr       */
+/*   Updated: 2020/05/18 01:17:44 by sedric           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_itoa(int n)
+char		*ft_itoa(int n)
 {
-	int arr[10];
-	int i;
-	int flag;
-	char *number;
-	
+	int		arr[10];
+	int		i;
+	int		flag;
+	char	*number;
+
 	flag = (n < 0 ? 1 : 0);
 	i = 0;
 	while (n != 0 || i == 0)
 	{
-		flag == 1 ? (arr[i] = n % 10 * -1) : (arr[i] = n % 10);
+		arr[i] = n % 10 * (1 - 2 * flag);
 		n = n / 10;
 		i++;
 	}
-	number = malloc(sizeof(char)*(i + 1 + flag));
-	if (number == NULL)
-			return (NULL);
-	if (flag == 1)
-	{
-		number[n] = '-';
-		n++;
-	}
+	if (!(number = malloc(sizeof(char) * (i + 1 + flag))))
+		return (NULL);
+	flag == 1 ? number[0] = '-' | n++ : 0;
 	while (i > 0)
 	{
 		number[n] = arr[i - 1] + 48;

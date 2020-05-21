178df68fe4e4bf796ab2e84455db5652ebc80a3d
Toka_toka <mos9101@mail.ru>
2020-05-22 01:08:09 +0300

    split - works, crash on 1 tets, norme - shit

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,78 +6,85 @@
 /*   By: sedric <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/06 17:13:04 by sedric            #+#    #+#             */
-/*   Updated: 2020/05/21 01:35:26 by sedric           ###   ########.fr       */
+/*   Updated: 2020/05/22 00:51:32 by sedric           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
-
-#include <stdio.h>
-
-/* int			ft_check (char const *s, char c)
-{
-	int		start;
-	
-	if (s == NULL)
-		return(-2);
-	while (s[start] == c)
-		start++;
-	if (s[start] == '\0')
-		return (-1);
-	return(start); 
-}	*/
+#include "libft.h"
 
 int ft_countword (char const *s, char c)
 {
-			int count;
+	int count;
 
-			count = 0;
-			while (*s != '\0')
-			{
-				while(*s == c)
-					s++;
-				if(*s != c && *s != '\0')
-					count++;
-				while(*s != c && *s != '\0')
-					s++;
-			}
-			return (count);
+	count = 0;
+	while (*s != '\0')
+	{
+		while(*s == c)
+			s++;
+		if(*s != c && *s != '\0')
+			count++;
+		while(*s != c && *s != '\0')
+			s++;
+	}
+	return (count);
 }
 
-/*char		**ft_createmassive(int words, int symbols)
+int		*ft_wordsearch(char const *s, int *arr, char c)
+{
+	arr[0] = arr[0] + arr[1];
+	arr[1] = 0;
+	while(s[arr[0]] == c)
+		arr[0]++;
+	while (s[arr[0] + arr[1]] != c && s[arr[0] + arr[1]] != '\0')
+		arr[1]++;
+	return(arr);
+}
+
+void	ft_cubalibre(int *app, char **split, int i)
+{
+	free (app);
+	while (i > 0 , i--)
+		free (split[i]);
+	free(split);
+}
+
+char		**ft_mallocmaster(int words, char const *s, char c)
+{
+	char			**split;
+	int				i;
+	int				*arr;
+	if (words == 0)
+		return(NULL);
+	i = 0;
+	arr = malloc(sizeof(int)*2);
+	arr[0] = 0;
+	arr[1] = 0;		
+	split = (char**)malloc((words + 1)* sizeof(char *));
+	if (split == NULL)
+		return (NULL);
+	while (i < words)
+	{
+		ft_wordsearch(s, arr, c);
+		split[i] = ft_substr(s, (unsigned int)arr[0], (size_t)arr[1]);
+		if (split[i] == NULL)
+		{	
+			ft_cubalibre(arr, split, i);
+				return (NULL);
+		}
+		i++;
+	}
+	split[words] = NULL;
+		return(split);
+}
+
+char		**ft_split(char const *s, char c)
 {
 	char	**split;
+	int		words;
 	
-	if (words == 0 && symbols == 0)
-	{
-		split = (char**)malloc(2 * sizeof(char *));
-		split[0] = (char *)malloc(sizeof(char) * 1);
-		split[0] = "\0";
-		split[1] = NULL;
-	}
-	else
-		return(split);*/
-//}
-
-int		ft_split(char const *s, char c)
-{
-//	char	**split;
-//	int		start;
-	int		count_word;
-//	size_t	len_begin;
-//	size_t	len_end;
-	
-//	start = ft_check (s, c);
-	count_word = ft_countword(s, c);
-	printf("word = %d", count_word);
-	
-/*	if (start == -2)
-		return (NULL);
-	if (start == -1)
-		split = ft_createmassive (0, 0);
-	else
-	{
-		ft_createmassive(s, start, 
-	}*/
-	return (0);// (split);
+	if (s == NULL)
+		return(NULL);
+	words = ft_countword(s, c);
+	split = ft_mallocmaster(words, s, c);
+	return (split);
 }

0b41c75ecb1624bda52493d60220f2dd718262c9
Toka_toka <mos9101@mail.ru>
2020-05-29 01:15:25 +0300

    strlcpy after check

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: sedric <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/06 17:13:04 by sedric            #+#    #+#             */
-/*   Updated: 2020/05/16 23:40:29 by sedric           ###   ########.fr       */
+/*   Updated: 2020/05/29 00:53:29 by sedric           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t len_dest)
 	if (dest == NULL || src == NULL)
 		return (0);
 	len_src = ft_strlen(src);
-	if (len_dest <= 0)
+	if (len_dest == 0)
 		return (len_src);
-	len_dest >= len_src ? len_dest = len_src : len_dest--;
-	while (count < len_dest)
+	len_dest--;
+	while (count < len_dest && src[count] != '\0')
 	{
 		dest[count] = src[count];
 		count++;
